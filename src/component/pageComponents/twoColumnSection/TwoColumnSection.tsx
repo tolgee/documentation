a3757027be8cce5ebe6b8d4b428f1584474ce83a
Jan Cizmar <cizmar@chlupac.com>
2022-08-02 11:00:28 +0200

    feat: Integrations

--- src/component/pageComponents/twoColumnSection/TwoColumnSection.tsx
+++ src/component/pageComponents/twoColumnSection/TwoColumnSection.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 export const TwoColumnSection: FC<{
   className?: string
 }> = ({children, className}) => <section
-  className={clsx(`flex items-center md:max-w-[1200px] mx-auto`, className)}>
+  className={clsx(`flex items-center justify-center md:max-w-[1200px] mx-auto`, className)}>
   <div className="grid md:grid-cols-2 gap-12 sm:m-12 m-8">
     {children}
   </div>
