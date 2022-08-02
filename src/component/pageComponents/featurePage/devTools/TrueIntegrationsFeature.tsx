b63d232633e7d25db3d83d3bd1622c8796d1b646
Jan Cizmar <cizmar@chlupac.com>
2022-08-02 11:15:00 +0200

    fix: Minor fixes

--- src/component/pageComponents/featurePage/devTools/TrueIntegrationsFeature.tsx
+++ src/component/pageComponents/featurePage/devTools/TrueIntegrationsFeature.tsx
@@ -4,16 +4,22 @@ import {FeatureHeader} from "../FeatureHeader";
 import {SingleColumnSection} from "../../twoColumnSection/SingleColumnSection";
 import {TechnologyIcons} from "../../../TechnologyIcons";
 
+export function TolgeeIsRealTexts() {
+  return <>
+    <TextColumnTitle>Tolgee is real</TextColumnTitle>
+    <TextColumnText>
+      Tolgee is not just other localization platform offering integrations which are just syncing you local data with the backend. Tolgee is really
+      integrated into your App via SDKs.
+    </TextColumnText>
+  </>;
+}
+
 export const TrueIntegrationsFeature = () => {
   return (
     <>
       <FeatureHeader title="True integrations" id="true-integrations"/>
       <SingleColumnSection>
-        <TextColumnTitle>Tolgee is real</TextColumnTitle>
-        <TextColumnText>
-          Tolgee is not just other localization platform offering integrations which are just syncing you local data with the backend. Tolgee is really
-          integrated into your App via SDKs.
-        </TextColumnText>
+        <TolgeeIsRealTexts/>
         <TechnologyIcons className="justify-center"/>
       </SingleColumnSection>
     </>)
