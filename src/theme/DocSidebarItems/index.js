b45310d8903d868b414f3cd9b43855c1efd06248
Štěpán Granát <granat.stepan@gmail.com>
2022-08-05 10:24:03 +0200

    feat: upgrade to docosarus 2.0.1 and components update

--- src/theme/DocSidebarItems/index.js
+++ src/theme/DocSidebarItems/index.js
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { DocSidebarItemsExpandedStateProvider } from '@docusaurus/theme-common';
+import { DocSidebarItemsExpandedStateProvider } from '@docusaurus/theme-common/internal';
 import DocSidebarItem from '@theme/DocSidebarItem';
 import SearchBar from '@theme/SearchBar';
 
@@ -9,20 +9,15 @@ function DocSidebarItems({ items, ...props }) {
   return (
     <DocSidebarItemsExpandedStateProvider>
       {props.level === 1 && (
-        <div className="sidebar-search py-2 [&>button]:bg-gray-100 [&>button]:dark:bg-black [&>button]:rounded-[10px]">
+        <div className="sidebar-search py-2 [&>button]:rounded-[10px]">
           <SearchBar />
         </div>
       )}
       {items.map((item, index) => (
-        <DocSidebarItem
-          key={index}
-          item={item}
-          index={index}
-          {...(props as any)}
-        />
+        <DocSidebarItem key={index} item={item} index={index} {...props} />
       ))}
     </DocSidebarItemsExpandedStateProvider>
   );
 }
-
+// Optimize sidebar at each "level"
 export default memo(DocSidebarItems);
