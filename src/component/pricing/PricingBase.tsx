301c26d6c8d986cc3ea053460b338e5579096e16
Štěpán Granát <granat.stepan@gmail.com>
2022-08-16 15:14:14 +0200

    feat: update pricing

--- src/component/pricing/PricingBase.tsx
+++ src/component/pricing/PricingBase.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { RoboMouse } from '../images/RoboMouse';
 import DecoratedLayout from '../../theme/DecoratedLayout';
 import { PageHeader } from '../pageComponents/header/PageHeader';
@@ -10,22 +11,15 @@ export const PricingBase: React.FC = ({ children }) => {
     <div className="text-home-text">
       <DecoratedLayout title={`Pricing`}>
         <PageHeader className="mb-[100px]">
-          <PageHeaderTitle active>Pricing (Preview)</PageHeaderTitle>
+          <PageHeaderTitle>Pricing</PageHeaderTitle>
         </PageHeader>
 
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center pb-20">
           <div className="pricing__container xl:max-w-[1200px]">
             {children}
 
             <div className="pricing__bottom-center-robomouse">
               <RoboMouse />
-            </div>
-
-            <div className="ml-[20px] text-lg my-16">
-              <p>
-                Currently, no limits are applied to translations count. We plan
-                to release pricing during July.
-              </p>
             </div>
           </div>
         </div>
