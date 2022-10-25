93ecaf0b25c8827d93ed4c7a7c39e792d5750b84
Štěpán Granát <granat.stepan@gmail.com>
2022-10-25 11:10:01 +0200

    feat: move redirects to separate folder

---
+++ redirects.js
@@ -0,0 +1,59 @@
+module.exports = [
+  // fixing legacy backlinks
+  {
+    from: '/docs/get_started/hello_world',
+    to: '/docs/',
+  },
+  {
+    from: '/docs/sdk/get_started/hello_world',
+    to: '/docs/sdk/about',
+  },
+  {
+    from: '/docs/changelog/version_3',
+    to: '/docs/changelog/tolgee_js_version_3',
+  },
+  {
+    from: '/docs/server_and_web_app/self_hosting/running_with_docker',
+    to: '/docs/platform/self_hosting/running_with_docker',
+  },
+  {
+    from: '/docs/sdk/introduction',
+    to: '/docs/sdk/about',
+  },
+  {
+    from: '/docs/web_app',
+    to: '/docs/sdk/about',
+  },
+  {
+    from: '/docs/sdk/understanding_tolgee_for_web',
+    to: '/docs/sdk/about',
+  },
+  {
+    from: '/docs/web_app/creating_repository',
+    to: '/docs/platform/creating_project',
+  },
+  {
+    from: '/docs/web_app/obtaining_api_key',
+    to: '/docs/platform/integration',
+  },
+  {
+    from: '/docs/sdk/integrations/react/react_preparing_for_production',
+    to: '/docs/sdk/integrations/react/installation',
+  },
+  {
+    from: '/docs/sdk/integrations/react/preparing_for_production',
+    to: '/docs/sdk/integrations/react/installation',
+  },
+  {
+    from: '/docs/server_and_web_app/self_hosting/configuration',
+    to: '/docs/platform/self_hosting/configuration',
+  },
+  {
+    from: '/tolgee-for-svelte',
+    to: '/integrations/svelte',
+  },
+  {
+    from: '/docs/platform/managing_api_keys',
+    to: '/docs/platform/api-keys-and-pat-tokens',
+  },
+];
