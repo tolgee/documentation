43d9559dfae3fc7fba077b715beaf8b77c2762a7
Štěpán Granát <granat.stepan@gmail.com>
2022-12-12 17:36:59 +0100

    feat: add docker tag to docs - fix active version missing

--- src/theme/CodeBlock/index.js
+++ src/theme/CodeBlock/index.js
@@ -3,13 +3,14 @@ import CodeBlock from '@theme-original/CodeBlock';
 import { useActivePluginAndVersion } from '@docusaurus/plugin-content-docs/client';
 
 export default function CodeBlockWrapper(props) {
-  const { activeVersion } = useActivePluginAndVersion();
-  const { label: versionLabel } = activeVersion;
-  const versionSegment = activeVersion.isLast ? '' : ':v' + versionLabel;
+  const activeVersion = useActivePluginAndVersion()?.activeVersion;
+  const versionLabel = activeVersion?.label;
+  const isLast = activeVersion?.isLast;
+  const versionSegment = !versionLabel || isLast ? '' : ':v' + versionLabel;
 
   const newProps = {
     ...props,
-    children: props.children?.replace(/{{dockerTagVersion}}/, versionSegment),
+    children: props.children?.replace?.(/{{dockerTagVersion}}/, versionSegment),
   };
 
   return (
