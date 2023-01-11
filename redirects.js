003f3927010d50839fab0f781843972e2f5ca316
Jan Cizmar <cizmar@chlupac.com>
2023-01-11 13:40:43 +0100

    feat: Switch to latest versions

--- redirects.js
+++ redirects.js
@@ -2,75 +2,84 @@ module.exports.redirects = {
   redirects: [
     // fixing legacy backlinks
     {
-      from: '/docs/get_started/hello_world',
-      to: '/js-sdk/get_started/hello_world',
+      from: "/docs/get_started/hello_world",
+      to: "/js-sdk/4.x.x/get_started/hello_world"
     },
     {
-      from: '/docs/changelog/version_3',
-      to: '/js-sdk/changelog/tolgee_js_version_3',
+      from: "/docs/changelog/version_3",
+      to: "/js-sdk/4.x.x/changelog/tolgee_js_version_3"
     },
     {
-      from: '/docs/changelog/tolgee_js_version_3',
-      to: '/js-sdk/changelog/tolgee_js_version_3',
+      from: "/docs/changelog/tolgee_js_version_3",
+      to: "/js-sdk/4.x.x/changelog/tolgee_js_version_3"
     },
     {
-      from: '/docs/server_and_web_app/self_hosting/running_with_docker',
-      to: '/platform/self_hosting/running_with_docker',
+      from: "/docs/server_and_web_app/self_hosting/running_with_docker",
+      to: "/platform/self_hosting/running_with_docker"
     },
     {
-      from: '/docs/web_app',
-      to: '/js-sdk/',
+      from: "/docs/web_app",
+      to: "/js-sdk/"
     },
     {
-      from: '/docs/web_app/creating_repository',
-      to: '/platform/creating_project',
+      from: "/docs/web_app/creating_repository",
+      to: "/platform/creating_project"
     },
     {
-      from: '/docs/web_app/obtaining_api_key',
-      to: '/platform/integration',
+      from: "/docs/web_app/obtaining_api_key",
+      to: "/platform/integration"
     },
     {
-      from: '/docs/web/using_with_react/react_preparing_for_production',
-      to: '/js-sdk/using_with_react/preparing_for_production',
+      from: "/docs/web/using_with_react/react_preparing_for_production",
+      to: "/js-sdk/integrations/react/installation"
     },
     {
-      from: '/docs/server_and_web_app/self_hosting/configuration',
-      to: '/platform/self_hosting/configuration',
+      from: "/docs/server_and_web_app/self_hosting/configuration",
+      to: "/platform/self_hosting/configuration"
     },
     {
-      from: '/tolgee-for-svelte',
-      to: '/integrations/svelte',
+      from: "/tolgee-for-svelte",
+      to: "/integrations/svelte"
     },
     {
-      from: '/platform/managing_api_keys',
-      to: '/platform/api-keys-and-pat-tokens',
+      from: "/platform/managing_api_keys",
+      to: "/platform/api-keys-and-pat-tokens"
     },
     {
-      from: '/docs/web/understanding_tolgee_for_web',
-      to: '/js-sdk/',
+      from: "/docs/web/understanding_tolgee_for_web",
+      to: "/js-sdk/"
     },
-    { from: '/docs/integrations', to: '/integrations/' },
-    { from: '/integrations/all', to: '/integrations/' },
-    { from: '/docs/icu_message_format', to: '/platform/icu_message_format' },
+    { from: "/docs/integrations", to: "/integrations/" },
+    { from: "/integrations/all", to: "/integrations/" },
+    { from: "/docs/icu_message_format", to: "/platform/icu_message_format" },
     {
-      from: '/server_and_web_app/self_hosting/configuration',
-      to: '/platform/self_hosting/configuration',
+      from: "/server_and_web_app/self_hosting/configuration",
+      to: "/platform/self_hosting/configuration"
     },
-    { from: '/docs/', to: '/platform/' },
+    { from: "/docs/", to: "/platform/" }
   ],
   createRedirects(existingPath) {
-    if (existingPath.indexOf('/platform') === 0) {
-      return [existingPath.replace('/platform', '/docs/platform')];
+    if (existingPath.indexOf("/js-sdk/5.x.x") === 0) {
+      return [
+        existingPath.replace("/js-sdk/5.x.x", "/js-sdk/5.0.0-alpha.2"),
+        existingPath.replace("/js-sdk/5.x.x", "/js-sdk/5.0.0-alpha.1")
+      ];
     }
-    if (existingPath.indexOf('/js-sdk') === 0) {
-      return [existingPath.replace('/js-sdk', '/docs/web')];
+    if (existingPath.indexOf("/js-sdk/4.x.x") === 0) {
+      return [existingPath.replace("/js-sdk/4.x.x", "/js-sdk")];
     }
-    if (existingPath.indexOf('/platform/3.0.0-alpha.6') === 0) {
-      return [existingPath.replace('/platform/3.0.0-alpha.6', '/platform/3.0.0-alpha.1')];
+    if (existingPath.indexOf("/js-sdk") === 0) {
+      return [existingPath.replace("/js-sdk", "/docs/web")];
     }
-    if (existingPath.indexOf('/js-sdk/5.0.0-alpha.2') === 0) {
-      return [existingPath.replace('/js-sdk/5.0.0-alpha.2', '/js-sdk/5.0.0-alpha.1')];
+    if (existingPath.indexOf("/platform/3.x.x") === 0) {
+      return [
+        existingPath.replace("/platform/3.x.x", "/platform/3.0.0-alpha.1"),
+        existingPath.replace("/platform/3.x.x", "/platform/3.0.0-alpha.6")
+      ];
+    }
+    if (existingPath.indexOf("/platform") === 0) {
+      return [existingPath.replace("/platform", "/docs/platform")];
     }
     return undefined;
-  },
+  }
 };
