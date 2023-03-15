66a20436cce55541f2d26b964a145583ed62f8b8
Štěpán Granát <granat.stepan@gmail.com>
2023-03-15 15:39:34 +0100

    feat: add authors description (#377)

---
+++ src/component/AuthorSummary.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Author from '@theme/BlogPostItem/Header/Author';
+
+type Author = {
+  description?: string;
+  imageURL: string;
+  key: string;
+  name: string;
+  title: string;
+  url: string;
+};
+
+type Props = {
+  author: Author;
+};
+
+export const AuthorSummary = ({ author }: Props) => {
+  return (
+    <div className="flex flex-col gap-2 my-4 px-4">
+      <Author author={author} />
+      {author.description && (
+        <div className="text-md">{author.description}</div>
+      )}
+    </div>
+  );
+};
