484d40f62d6042d895ab74b9e7d03b0131805696
Štěpán Granát <granat.stepan@gmail.com>
2023-04-28 14:44:10 +0200

    fix: gtag script (#403)

--- src/component/externalScripts/getGtagScript.ts
+++ src/component/externalScripts/getGtagScript.ts
@@ -1,20 +1,17 @@
 export const getGtagScript = (trackingId: string) => {
-  return `window.dataLayer = window.dataLayer || [];
+  return `
+    window.dataLayer = window.dataLayer || [];
 
-            window.gtag = function (){
-            dataLayer.push(arguments);
-            }
-            
-            gtag('consent', 'default', {
-              'analytics_storage': 'denied',
-              'ad_storage': 'denied'
-            });
+    window.gtag = function (){
+      dataLayer.push(arguments);
+    }
 
-            gtag('js', new Date());
+    gtag('js', new Date());
 
-            if(typeof window.onGtagLoaded === 'function'){
-              window.onGtagLoaded();
-            }
+    if(typeof window.onGtagLoaded === 'function'){
+      window.onGtagLoaded();
+    }
 
-            gtag('config', '${trackingId}');`;
+    gtag('config', '${trackingId}');
+  `;
 };
