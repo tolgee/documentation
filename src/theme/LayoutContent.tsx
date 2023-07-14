fe417b4088bc6463e0d73074286d657626996218
Jan Cizmar <cizmar@chlupac.com>
2023-07-14 14:31:31 +0200

    fix: Remove cookie consent

--- src/theme/LayoutContent.tsx
+++ src/theme/LayoutContent.tsx
@@ -44,13 +44,13 @@ export const LayoutContent = ({ children }) => {
   return (
     <>
       <Head>
-        {cookieYesId && (
-          <script
-            id="cookieyes"
-            type="text/javascript"
-            src={`https://cdn-cookieyes.com/client_data/${cookieYesId}/script.js`}
-          ></script>
-        )}
+        {/*{cookieYesId && (*/}
+        {/*  <script*/}
+        {/*    id="cookieyes"*/}
+        {/*    type="text/javascript"*/}
+        {/*    src={`https://cdn-cookieyes.com/client_data/${cookieYesId}/script.js`}*/}
+        {/*  ></script>*/}
+        {/*)}*/}
         {trackingId && (
           <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
