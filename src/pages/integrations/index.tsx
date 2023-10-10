7ed66694b0f9a90a26291ad950e76cb96f1855d7
Štěpán Granát <granat.stepan@gmail.com>
2023-10-10 10:00:56 +0200

    feat: add integrations description (#474)

--- src/pages/integrations/index.tsx
+++ src/pages/integrations/index.tsx
@@ -24,11 +24,32 @@ export default function Integrations() {
         title={baseSchema.slogan}
         description={baseSchema.description}
       >
-        <PageHeader className="mb-[180px]">
+        <PageHeader className="mb-[100px]">
           <PageHeaderSubtitle h1>Integrations</PageHeaderSubtitle>
           <PageHeaderTitle active>Setup in seconds.</PageHeaderTitle>
         </PageHeader>
+
         <div className="md:max-w-[1000px] mx-auto sm:max-w-[650px] max-w-[400px]">
+          <div className="mb-20 px-6 text-lg font-medium">
+            <p>
+              Become unbeatable in app localization with Tolgee! Our native
+              JavaScript integrations empower you to:
+            </p>
+            <ul>
+              <li>
+                Edit translations directly in your app. No developer knowledge
+                needed for anyone to make changes.
+              </li>
+              <li>
+                Create one-click screenshots for translators to give them extra
+                context for accurate translations.
+              </li>
+              <li>
+                Significantly enhance machine translation quality as Tolgee
+                gathers context from your app.
+              </li>
+            </ul>
+          </div>
           <div className="grid md:grid-cols-3 sm:grid-cols-2 mx-5 gap-5">
             {integrations.map((integration) => (
               <IntegrationTile
