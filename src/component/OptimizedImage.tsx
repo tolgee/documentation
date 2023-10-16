c735a079c492a4d8b52356854117dfa3e7e1bf4d
Štěpán Granát <granat.stepan@gmail.com>
2023-10-16 15:16:11 +0200

    fix: dark images in dark mode

--- src/component/OptimizedImage.tsx
+++ src/component/OptimizedImage.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import ThemedImage from '@theme/ThemedImage';
-import { useDarkMode } from '../utils';
 
-type Props = {
-  webp?: boolean;
-} & React.ComponentProps<typeof ThemedImage>;
+type Props = React.ComponentProps<typeof ThemedImage>;
 
-export const OptimizedImage: React.FC<Props> = ({ sources, webp, ...rest }) => {
-  const isDark = useDarkMode();
-  const url =
-    (isDark ? sources.dark : sources.light) || sources.light || sources.dark;
-
+export const OptimizedImage: React.FC<Props> = ({ sources, ...rest }) => {
   return (
-    <picture style={{ display: 'contents' }}>
-      {webp && (
-        <source srcSet={url.replace(/\..*$/g, '.webp')} type="image/webp" />
-      )}
-      <img src={url} {...rest} />
-    </picture>
+    <ThemedImage
+      sources={{
+        light: sources.light,
+        dark: sources.dark,
+      }}
+      {...rest}
+    />
   );
 };
