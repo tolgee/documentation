cd916aa52e38bac6152c78ad2c184faf0538f80c
Jan Cizmar <cizmar@chlupac.com>
2023-11-23 18:16:29 +0100

    feat: Webhooks and content delivery fixes

--- sidebarPlatform.js
+++ sidebarPlatform.js
@@ -17,9 +17,10 @@ module.exports = {
         'projects_and_organizations/languages',
         'projects_and_organizations/members',
         'projects_and_organizations/import',
-        'projects_and_organizations/content_delivery',
         'projects_and_organizations/export',
         'projects_and_organizations/organization_settings',
+        'projects_and_organizations/content_delivery',
+        'projects_and_organizations/webhooks',
       ],
     },
     {
