0c809b8abce483ae6da584aa4c2a4f94fcf15f32
Jan Cizmar <cizmar@chlupac.com>
2024-01-18 15:59:52 +0100

    fix: New pricing -> blog post

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,12 +1,10 @@
 export type CustomThemeColors = typeof lightThemeColors;
 export const lightThemeColors = {
-  tableHeadBackground: 'var(--overlay-background)',
+  tableHeadBackground: 'var(--table-head-background)',
   tableBorder: 'var(--table-border-color)',
   tableRowBackground: 'var(--ifm-background-color)',
 };
 
 export const darkThemeColors = {
-  tableHeadBackground: 'var(--overlay-background)',
-  tableBorder: 'var(--table-border-color)',
-  tableRowBackground: 'var(--ifm-background-color)',
+  ...lightThemeColors,
 } as CustomThemeColors;
