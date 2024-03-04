069397ca59502630e652ff275ca3166353a4d752
Jan Cizmar <cizmar@chlupac.com>
2024-03-04 20:45:24 +0100

    docs: Formats and ICU placeholders docs

--- src/component/docs/formats/PlaceholderConversions.tsx
+++ src/component/docs/formats/PlaceholderConversions.tsx
@@ -24,20 +24,13 @@ export const PlaceholderConversion: FC<{
   originalName: ReactNode;
   target: 'original' | 'icu';
 }> = (props) => {
-  const tolgeeIcuHeader = 'Univeral ICU';
-
-  const first =
-    props.target === 'original' ? props.originalName : tolgeeIcuHeader;
-  const second =
-    props.target === 'original' ? tolgeeIcuHeader : props.originalName;
-
   return (
     <Table>
       <TableHead>
         <TableRow>
           <TableHeadCell className="text-left">Specifier</TableHeadCell>
           <TableHeadCell className="text-left">ICU type</TableHeadCell>
-          <TableHeadCell className="text-left">Apple Example</TableHeadCell>
+          <TableHeadCell className="text-left">Example</TableHeadCell>
           <TableHeadCell className="text-left">Converted to ICU</TableHeadCell>
           <TableHeadCell className="text-left">Note</TableHeadCell>
         </TableRow>
