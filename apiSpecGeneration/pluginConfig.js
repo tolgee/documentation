40f7e7ed35609d79d9c56c189f5f14b9471c9498
Jan Cizmar <cizmar@chlupac.com>
2024-04-05 17:15:04 +0200

    chore: Clean

--- apiSpecGeneration/pluginConfig.js
+++ apiSpecGeneration/pluginConfig.js
@@ -5,17 +5,13 @@ module.exports = [
     docsPluginId: 'rest-api',
     config: {
       tolgee: {
-        specPath:
-          './apiSpecGeneration/apiSpecs.json', // path or URL to the OpenAPI spec
+        specPath: './apiSpecGeneration/apiSpecs.json', // path or URL to the OpenAPI spec
         outputDir: 'api', // output directory for generated *.mdx and sidebar.js files
         sidebarOptions: {
           categoryLinkSource: 'tag',
           sidebarCollapsible: false,
           sidebarCollapsed: false,
           groupPathsBy: 'tag', // generate a sidebar.js slice that groups operations by tag
-        },
-        markdownGenerators: {
-          // createApiPageMD: customCreateApiPageMD,
         },
       },
     },
