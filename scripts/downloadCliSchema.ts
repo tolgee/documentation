c5501f3222160f6795560b76441aef643cee5043
Štěpán Granát <granat.stepan@gmail.com>
2024-05-31 09:31:51 +0200

    feat: add cli schema

---
+++ scripts/downloadCliSchema.ts
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+
+async function downloadOssData() {
+  const response = await fetch(
+    'https://raw.githubusercontent.com/tolgee/tolgee-cli/main/schema.json'
+  );
+  const json = await response.json();
+  fs.writeFileSync(
+    path.join(__dirname, '../static/cli-schema.json'),
+    JSON.stringify(json, null, 2)
+  );
+}
+
+downloadOssData();
