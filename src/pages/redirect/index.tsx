2914794c05de7fae01a670fffcb4e345e5e10af9
Jan Cizmar <cizmar@chlupac.com>
2024-10-07 19:30:02 +0200

    feat: Add plausible tracking and trigger event

--- src/pages/redirect/index.tsx
+++ src/pages/redirect/index.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { usePlausible } from '../../component/externalScripts/plausible';
+import { useHistory } from 'react-router-dom';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export default function Index() {
-  const { search } = window.location;
-  const targetUrl = new URLSearchParams(search).get('to');
+export default function Redirect() {
+  return (
+    <BrowserOnly>
+      {() => {
+        const { search } = useHistory().location;
+        const targetUrl = new URLSearchParams(search).get('to');
 
-  const redirect = () => (window.location.href = targetUrl);
+        const redirect = () => (window.location.href = targetUrl);
 
-  usePlausible((plausible) => {
-    plausible('External Redirect', {
-      callback: () => {
-        redirect();
-      },
-      props: { targetUrl },
-    });
-  });
+        usePlausible((plausible) => {
+          plausible('External Redirect', {
+            callback: () => {
+              redirect();
+            },
+            props: { targetUrl },
+          });
+        });
 
-  setTimeout(redirect, 5000);
+        setTimeout(redirect, 5000);
 
-  return <>Redirecting you to {targetUrl}!</>;
+        return <>Redirecting you to {targetUrl}!</>;
+      }}
+    </BrowserOnly>
+  );
 }
