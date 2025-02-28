71c4d43e80e07bb9e2626d06aa1c849e0b542e36
Štěpán Granát <granat.stepan@gmail.com>
2025-02-28 14:11:39 +0100

    feat: some swizzling

--- src/component/AuthorSummary.tsx
+++ src/component/AuthorSummary.tsx
@@ -1,23 +1,14 @@
+import Author from '@theme/Blog/Components/Author';
 import React from 'react';
-// import Author from '@theme/BlogPostItem/Header/Author';
-
-export type AuthorType = {
-  description?: React.ReactNode;
-  imageURL: string;
-  key?: string;
-  name: string;
-  title: string;
-  url?: string;
-};
 
 type Props = {
-  author: AuthorType;
+  author: React.ComponentProps<typeof Author>['author'];
 };
 
 export const AuthorSummary = ({ author }: Props) => {
   return (
     <div className="flex flex-col gap-2 my-4 px-4">
-      {/* <Author author={author} /> */}
+      <Author author={author} />
       {author.description && (
         <div className="text-md">{author.description}</div>
       )}
---
+++ src/theme/DocSidebarItem/Link/index.tsx
@@ -0,0 +1,60 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import { ThemeClassNames } from '@docusaurus/theme-common';
+import { isActiveSidebarItem } from '@docusaurus/plugin-content-docs/client';
+import Link from '@docusaurus/Link';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import IconExternalLink from '@theme/Icon/ExternalLink';
+import type { Props } from '@theme/DocSidebarItem/Link';
+
+import styles from './styles.module.css';
+import { Badges } from '../../../component/docs/apiDocs/Badges';
+
+export default function DocSidebarItemLink({
+  item,
+  onItemClick,
+  activePath,
+  level,
+  index,
+  ...props
+}: Props): ReactNode {
+  const { href, label, className, autoAddBaseUrl } = item;
+  const isActive = isActiveSidebarItem(item, activePath);
+  const isInternalLink = isInternalUrl(href);
+  return (
+    <li
+      className={clsx(
+        ThemeClassNames.docs.docSidebarItemLink,
+        ThemeClassNames.docs.docSidebarItemLinkLevel(level),
+        'menu__list-item',
+        className
+      )}
+      key={label}
+    >
+      <Link
+        className={clsx(
+          'menu__link',
+          !isInternalLink && styles.menuExternalLink,
+          {
+            'menu__link--active': isActive,
+          }
+        )}
+        autoAddBaseUrl={autoAddBaseUrl}
+        aria-current={isActive ? 'page' : undefined}
+        to={href}
+        {...(isInternalLink && {
+          onClick: onItemClick ? () => onItemClick(item) : undefined,
+        })}
+        {...props}
+      >
+        <span>
+          {label}
+          {item.customProps?.apiDocsBadges && (
+            <Badges badges={item.customProps?.apiDocsBadges as any} />
+          )}
+          {!isInternalLink && <IconExternalLink />}
+        </span>
+      </Link>
+    </li>
+  );
+}
