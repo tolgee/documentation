f0ddec12827ef455117fb1aeb00978fa288be75e
Tolgee Machine <machine@tolgee.io>
2025-09-30 19:15:35 +0000

    feat: Update Generated code to platform version v3.132.4.1

--- api/sidebar.ts
+++ api/sidebar.ts
@@ -1304,6 +1304,36 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
+      label: "Export",
+      link: {
+        type: "doc",
+        id: "export",
+      },
+      collapsible: false,
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "do-export-json-zip",
+          label: "Export to ZIP of jsons",
+          className: "menu__list-item--deprecated api-method get",
+        },
+        {
+          type: "doc",
+          id: "export-data",
+          label: "Export data",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "export-post",
+          label: "Export data (post)",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Ai prompt controller",
       link: {
         type: "doc",
@@ -1359,36 +1389,6 @@ const sidebar: SidebarsConfig = {
           id: "delete-prompt",
           label: "Delete prompt",
           className: "api-method delete",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "Export",
-      link: {
-        type: "doc",
-        id: "export",
-      },
-      collapsible: false,
-      collapsed: false,
-      items: [
-        {
-          type: "doc",
-          id: "do-export-json-zip",
-          label: "Export to ZIP of jsons",
-          className: "menu__list-item--deprecated api-method get",
-        },
-        {
-          type: "doc",
-          id: "export-data",
-          label: "Export data",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "export-post",
-          label: "Export data (post)",
-          className: "api-method post",
         },
       ],
     },
