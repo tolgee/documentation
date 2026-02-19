1a9fd98a4a0238d09749fd5312b669158eb6037a
Tolgee Machine <machine@tolgee.io>
2026-02-19 11:32:07 +0000

    chore: Update generated docs to align with v3.160.0

--- api/sidebar.ts
+++ api/sidebar.ts
@@ -1388,54 +1388,6 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Llm providers",
-      link: {
-        type: "doc",
-        id: "tag_llm-providers",
-      },
-      collapsible: false,
-      collapsed: false,
-      items: [
-        {
-          type: "doc",
-          id: "get-all-11",
-          label: "Get all organization-specific providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "create-provider",
-          label: "Create organization-specific provider",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "get-available-providers",
-          label: "Get all available llm providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "get-server-providers",
-          label: "Get all server-configured providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "update-provider",
-          label: "Update organization-specific provider",
-          className: "api-method put",
-        },
-        {
-          type: "doc",
-          id: "delete-provider",
-          label: "Delete organization-specific provider",
-          className: "api-method delete",
-        },
-      ],
-    },
-    {
-      type: "category",
       label: "Projects",
       link: {
         type: "doc",
@@ -1538,19 +1490,49 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Ai Playground result controller",
+      label: "Llm providers",
       link: {
         type: "doc",
-        id: "tag_ai-playground-result-controller",
+        id: "tag_llm-providers",
       },
       collapsible: false,
       collapsed: false,
       items: [
         {
           type: "doc",
-          id: "get-ai-playground-result",
-          label: "Get ai playground result",
+          id: "get-all-11",
+          label: "Get all organization-specific providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "create-provider",
+          label: "Create organization-specific provider",
           className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "get-available-providers",
+          label: "Get all available llm providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "get-server-providers",
+          label: "Get all server-configured providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "update-provider",
+          label: "Update organization-specific provider",
+          className: "api-method put",
+        },
+        {
+          type: "doc",
+          id: "delete-provider",
+          label: "Delete organization-specific provider",
+          className: "api-method delete",
         },
       ],
     },
@@ -1580,6 +1562,24 @@ const sidebar: SidebarsConfig = {
           type: "doc",
           id: "export-post",
           label: "Export data (post)",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Ai Playground result controller",
+      link: {
+        type: "doc",
+        id: "tag_ai-playground-result-controller",
+      },
+      collapsible: false,
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "get-ai-playground-result",
+          label: "Get ai playground result",
           className: "api-method post",
         },
       ],
