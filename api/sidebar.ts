68489a8675e07ac8daae199fc43fb6635d6966ab
Tolgee Machine <machine@tolgee.io>
2026-05-13 13:10:53 +0000

    chore: Update generated docs to align with v3.186.1

--- api/sidebar.ts
+++ api/sidebar.ts
@@ -1364,6 +1364,36 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
+      label: "Export",
+      link: {
+        type: "doc",
+        id: "tag_export",
+      },
+      collapsible: false,
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "do-export-json-zip",
+          label: "Export to ZIP of jsons",
+          className: "menu__list-item--deprecated api-method get",
+        },
+        {
+          type: "doc",
+          id: "export-data",
+          label: "Export data",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "export-post",
+          label: "Export data (post)",
+          className: "api-method post",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Projects",
       link: {
         type: "doc",
@@ -1466,54 +1496,6 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Llm providers",
-      link: {
-        type: "doc",
-        id: "tag_llm-providers",
-      },
-      collapsible: false,
-      collapsed: false,
-      items: [
-        {
-          type: "doc",
-          id: "get-all-11",
-          label: "Get all organization-specific providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "create-provider",
-          label: "Create organization-specific provider",
-          className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "get-available-providers",
-          label: "Get all available llm providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "get-server-providers",
-          label: "Get all server-configured providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "update-provider",
-          label: "Update organization-specific provider",
-          className: "api-method put",
-        },
-        {
-          type: "doc",
-          id: "delete-provider",
-          label: "Delete organization-specific provider",
-          className: "api-method delete",
-        },
-      ],
-    },
-    {
-      type: "category",
       label: "Ai prompt controller",
       link: {
         type: "doc",
@@ -1574,31 +1556,49 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Export",
+      label: "Llm providers",
       link: {
         type: "doc",
-        id: "tag_export",
+        id: "tag_llm-providers",
       },
       collapsible: false,
       collapsed: false,
       items: [
         {
           type: "doc",
-          id: "do-export-json-zip",
-          label: "Export to ZIP of jsons",
-          className: "menu__list-item--deprecated api-method get",
-        },
-        {
-          type: "doc",
-          id: "export-data",
-          label: "Export data",
+          id: "get-all-11",
+          label: "Get all organization-specific providers",
           className: "api-method get",
         },
         {
           type: "doc",
-          id: "export-post",
-          label: "Export data (post)",
+          id: "create-provider",
+          label: "Create organization-specific provider",
           className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "get-available-providers",
+          label: "Get all available llm providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "get-server-providers",
+          label: "Get all server-configured providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "update-provider",
+          label: "Update organization-specific provider",
+          className: "api-method put",
+        },
+        {
+          type: "doc",
+          id: "delete-provider",
+          label: "Delete organization-specific provider",
+          className: "api-method delete",
         },
       ],
     },
