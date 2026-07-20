13143bef71735f916d9c460df82cdb1e96d87fae
Tolgee Machine <machine@tolgee.io>
2026-07-20 14:29:23 +0000

    chore: Update generated docs to align with v3.214.0

--- api/sidebar.ts
+++ api/sidebar.ts
@@ -1394,6 +1394,54 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
+      label: "Llm providers",
+      link: {
+        type: "doc",
+        id: "tag_llm-providers",
+      },
+      collapsible: false,
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "get-all-12",
+          label: "Get all organization-specific providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "create-provider",
+          label: "Create organization-specific provider",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "get-available-providers",
+          label: "Get all available llm providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "get-server-providers",
+          label: "Get all server-configured providers",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "update-provider",
+          label: "Update organization-specific provider",
+          className: "api-method put",
+        },
+        {
+          type: "doc",
+          id: "delete-provider",
+          label: "Delete organization-specific provider",
+          className: "api-method delete",
+        },
+      ],
+    },
+    {
+      type: "category",
       label: "Ai prompt controller",
       link: {
         type: "doc",
@@ -1454,49 +1502,31 @@ const sidebar: SidebarsConfig = {
     },
     {
       type: "category",
-      label: "Llm providers",
+      label: "Export",
       link: {
         type: "doc",
-        id: "tag_llm-providers",
+        id: "tag_export",
       },
       collapsible: false,
       collapsed: false,
       items: [
         {
           type: "doc",
-          id: "get-all-12",
-          label: "Get all organization-specific providers",
+          id: "do-export-json-zip",
+          label: "Export to ZIP of jsons",
+          className: "menu__list-item--deprecated api-method get",
+        },
+        {
+          type: "doc",
+          id: "export-data",
+          label: "Export data",
           className: "api-method get",
         },
         {
           type: "doc",
-          id: "create-provider",
-          label: "Create organization-specific provider",
+          id: "export-post",
+          label: "Export data (post)",
           className: "api-method post",
-        },
-        {
-          type: "doc",
-          id: "get-available-providers",
-          label: "Get all available llm providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "get-server-providers",
-          label: "Get all server-configured providers",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "update-provider",
-          label: "Update organization-specific provider",
-          className: "api-method put",
-        },
-        {
-          type: "doc",
-          id: "delete-provider",
-          label: "Delete organization-specific provider",
-          className: "api-method delete",
         },
       ],
     },
@@ -1599,36 +1629,6 @@ const sidebar: SidebarsConfig = {
           id: "set-users-permissions-1",
           label: "Set direct permission to user",
           className: "api-method put",
-        },
-      ],
-    },
-    {
-      type: "category",
-      label: "Export",
-      link: {
-        type: "doc",
-        id: "tag_export",
-      },
-      collapsible: false,
-      collapsed: false,
-      items: [
-        {
-          type: "doc",
-          id: "do-export-json-zip",
-          label: "Export to ZIP of jsons",
-          className: "menu__list-item--deprecated api-method get",
-        },
-        {
-          type: "doc",
-          id: "export-data",
-          label: "Export data",
-          className: "api-method get",
-        },
-        {
-          type: "doc",
-          id: "export-post",
-          label: "Export data (post)",
-          className: "api-method post",
         },
       ],
     },
